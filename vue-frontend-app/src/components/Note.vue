3da10abddfe06018585d33df5e1460ea4d40f124
Ebenezer Awotoro <ebenco94@gmail.com>
2023-09-28 12:55:25 +0200

    Reworking note and tag component

--- vue-frontend-app/src/components/Note.vue
+++ vue-frontend-app/src/components/Note.vue
@@ -1,52 +1,149 @@
 <template>
-  <div>
-    <h2>Notes</h2>
-    <ul>
-      <li v-for="(note, index) in notes" :key="index">
-        {{ note.text }}
-        <button @click="deleteNote(index)">Delete</button>
-        <div>
-          <label for="tag">Add Tag:</label>
-          <input type="text" v-model="newTag" @keydown.enter="addTag(index)" />
+    <div class="annotate">
+        <div class="new-note" @click="newNote">Add new note</div>
+        <ul class="notes">
+            <li class="note" v-for="noteItem in notes" :key="noteItem.id" v-text="noteItem.title" @click="edit(noteItem)"></li>
+        </ul>
+        <div class="form">
+            <input type="hidden" name="id" v-model="note.id">
+            <input type="text" name="title" v-model="note.title">
+            <textarea name="body" v-model="note.body"></textarea>
+            <button @click="save">Save</button>
         </div>
-        <div v-for="tag in note.tags" :key="tag">
-          {{ tag }}
-        </div>
-      </li>
-    </ul>
-    <div>
-      <label for="note">Add Note:</label>
-      <input type="text" v-model="newNote" @keydown.enter="addNote" />
     </div>
-  </div>
 </template>
+
 <script>
-export default {
-    name: "NoteComponent",
-  data() {
-    return {
-      notes: [],
-      newNote: "",
-      newTag: "",
-    };
-  },
-  methods: {
-    addNote() {
-      if (this.newNote.trim() !== "") {
-        this.notes.push({ text: this.newNote, tags: [] });
-        this.newNote = "";
-      }
-    },
-    deleteNote(index) {
-      this.notes.splice(index, 1);
-    },
-    addTag(noteIndex) {
-      const tag = this.newTag.trim();
-      if (tag !== "") {
-        this.notes[noteIndex].tags.push(tag);
-        this.newTag = "";
-      }
-    },
-  },
-};
+    export default {
+        name: "NoteCompnent",
+        data() {
+            return {
+                note: {
+                    id: "",
+                    title: "",
+                    body: ""
+                },
+                notes: []
+            };
+        },
+        methods: {
+            save() {
+                let note = this.note;
+                if(note.id !== "") {
+                    this.notes = this.notes.map(item => {
+                        if(item.id === note.id) {
+                            return note;
+                        }
+                        return item;
+                    });
+                } else {
+                    note.id = (new Date).getTime();
+                    this.notes.push(note);
+                }
+            },
+            edit(note) {
+                this.note = note;
+            },
+            newNote() {
+                this.note = {
+                    id: "",
+                    title: "",
+                    body: ""
+                };
+            }
+        }
+    }
 </script>
+/** @test */
+    Unite.test("It displays all the notes", () => {
+        addNote("Note 1", "Nody body");
+        addNote("Note 2", "Nody body");
+
+        let notes = wrapper.find(".notes");
+        expect(notes.vnode.children.length).toEqual(2);
+    });
+
+    /** @test */
+    Unite.test("It sees the form to create/edit notes", () => {
+        let form = wrapper.find(".form");
+        
+        expect(form.find("input[name=id]").exists()).toBe(true);
+        expect(form.find("input[name=title]").exists()).toBe(true);
+        expect(form.find("textarea[name=body]").exists()).toBe(true);
+        expect(form.find("button").exists()).toBe(true);
+    });
+
+    /** @test */
+    Unite.test("It adds a new note when the button is clicked", () => {
+        type("input[name=title]", "New Note");
+        type("textarea[name=body]", "New note body");
+        
+        expect(wrapper.find(".notes").isEmpty()).toBe(true);
+
+        wrapper.find("button").trigger("click");
+
+        expect(wrapper.find(".notes").vnode.children.length).toEqual(1);
+        expect(wrapper.find(".note:first-child").text()).toEqual("New Note");
+    });
+
+    /** @test */
+    Unite.test("It shows the clicked note on the form", () => {
+        let note = addNote("Note 1", "Note's body");
+
+        wrapper.find(".note:first-child").trigger("click");
+        
+        let form = wrapper.find(".form");
+        expect(form.find("input[name=id]").element.value).toEqual(String(note.id));
+        expect(form.find("input[name=title]").element.value).toEqual(note.title);
+        expect(form.find("textarea[name=body]").element.value).toEqual(note.body);
+    });
+
+    /** @test */
+    Unite.test("It updates a note object with the input data", () => {
+        addNote("Note 1", "Note's body");
+        addNote("Note 2", "Note's body");
+        addNote("Note 3", "Note's body");
+
+        wrapper.find(".note:nth-child(2)").trigger("click");
+        type("input[name=title]", "New Title");
+        wrapper.find("button").trigger("click");
+
+        expect(wrapper.vm.notes.length).toEqual(3);
+        expect(wrapper.find(".note:nth-child(2)").text()).toEqual("New Title");
+    });
+
+    /** @test */
+    Unite.test("It shows the blank form when the new button is clicked", () => {
+        addNote("Note Title", "Note's body");
+        wrapper.find(".note:first-child").trigger("click");
+
+        expect(wrapper.find("input[name=title]").element.value).toEqual("Note Title");
+
+        wrapper.find(".new-note").trigger("click");
+
+        expect(wrapper.find("input[name=id]").element.value).toEqual("");
+        expect(wrapper.find("input[name=title]").element.value).toEqual("");
+        expect(wrapper.find("textarea[name=body]").element.value).toEqual("");
+    });
+
+    /** @helper It adds a new Note */
+    let addNote = (title, body) => {
+        let noteKey = wrapper.vm.notes.length;
+        wrapper.vm.notes.push({
+            id: (new Date).getTime(),
+            title,
+            body
+        });
+        wrapper.update();
+
+        return wrapper.vm.notes[noteKey];
+    };
+
+    /** @helper It types into an input */
+    let type = (selector, value) => {
+        let node = wrapper.find(selector);
+        
+        node.element.value = value;
+        node.trigger("input");
+    };
+</tests>
--- vue-frontend-app/src/components/Tag.vue
+++ vue-frontend-app/src/components/Tag.vue
@@ -1,39 +1,135 @@
 <template>
-  <div>
-    <h2>Tags</h2>
-    <ul>
-      <li v-for="(tag, index) in tags" :key="index">
+  <div class="tag-input">
+    <div class="selected-tags">
+      <div
+        v-for="(tag, index) in selectedTags"
+        :key="index"
+        class="selected-tag"
+      >
         {{ tag }}
-        <button @click="deleteTag(index)">Delete</button>
-      </li>
-    </ul>
-    <div>
-      <label for="tag">Add Tag:</label>
-      <input type="text" v-model="newTag" @keydown.enter="addTag" />
+        <span @click="removeTag(index)" class="remove-tag-btn">×</span>
+      </div>
+    </div>
+    <input
+      type="text"
+      v-model="newTag"
+      @keydown.enter="addTag"
+      @input="searchTags"
+      placeholder="Add a tag"
+    />
+    <div class="tag-list" v-show="showTagList">
+      <div
+        v-for="(tag, index) in filteredTagList"
+        :key="index"
+        @click="selectTag(tag)"
+        class="tag-item"
+      >
+        {{ tag }}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-    name: "TagComponent",
+  name: "TagCompnent",
   data() {
     return {
-      tags: [],
       newTag: "",
+      selectedTags: [],
+      tagList: ["Tag 1", "Tag 2", "Tag 3", "Tag 4"], // Replace with your tag list data
+      showTagList: false,
     };
+  },
+  computed: {
+    filteredTagList() {
+      return this.tagList.filter((tag) =>
+        tag.toLowerCase().includes(this.newTag.toLowerCase())
+      );
+    },
   },
   methods: {
     addTag() {
       const tag = this.newTag.trim();
-      if (tag !== "") {
-        this.tags.push(tag);
+      if (tag !== "" && !this.selectedTags.includes(tag)) {
+        this.selectedTags.push(tag);
         this.newTag = "";
+        this.showTagList = false;
       }
     },
-    deleteTag(index) {
-      this.tags.splice(index, 1);
+    removeTag(index) {
+      this.selectedTags.splice(index, 1);
+    },
+    searchTags() {
+      this.showTagList = true;
+    },
+    selectTag(tag) {
+      if (!this.selectedTags.includes(tag)) {
+        this.selectedTags.push(tag);
+        this.newTag = "";
+      }
+      this.showTagList = false;
     },
   },
 };
 </script>
+
+<style>
+.tag-input {
+  position: relative;
+}
+
+.selected-tags {
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 8px;
+}
+
+.selected-tag {
+  background-color: #007bff;
+  color: #fff;
+  padding: 3px 8px;
+  margin-right: 5px;
+  margin-bottom: 5px;
+  border-radius: 3px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.remove-tag-btn {
+  margin-left: 5px;
+  cursor: pointer;
+}
+
+input[type="text"] {
+  width: 100%;
+  padding: 5px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  font-size: 14px;
+  outline: none;
+}
+
+.tag-list {
+  position: absolute;
+  top: 100%;
+  left: 0;
+  right: 0;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-top: none;
+  max-height: 150px;
+  overflow-y: auto;
+}
+
+.tag-item {
+  padding: 5px;
+  cursor: pointer;
+}
+
+.tag-item:hover {
+  background-color: #f0f0f0;
+}
+</style>
